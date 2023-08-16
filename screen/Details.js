72da4b2f6ceed3c1982cef44eaea33fedd8dab8e
gustavodepaulasilva2 <99282594+gustavodepaulasilva2@users.noreply.github.com>
2023-08-16 14:46:16 -0300

    Add files via upload

---
+++ screen/Details.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet, } from "react-native"
+
+export default class DetailScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Tela de detales</Text>
+            </View>
+        )
+    }
+}
